0b80b72c03a968a8b02fe37d3cc512d380a4212b
Luciano D. Perez <lucianoperezvic84@gmail.com>
2024-08-11 23:46:17 -0300

    features

---
+++ back/app/Http/Actions/ShopAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Actions;
+
+use App\Http\Controllers\Api\ActionInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+class DollarAction implements ActionInterface
+{
+    public function execute(Request $request)
+    {
+        try {
+            $response = Http::get('https://dolarapi.com/v1/dolares');
+
+            if ($response->successful()) {
+                $data = $response->json();
+
+                $formattedText = array_map(function ($rate) {
+                    return "Tipo de dólar: {$rate['nombre']}\n" .
+                        "Compra: {$rate['compra']}, Venta: {$rate['venta']}\n" .
+                        "Fecha de actualización: " . date('d-m-Y H:i:s', strtotime($rate['fechaActualizacion'])) . "\n";
+                }, $data);
+
+                // Unir los textos y devolver como respuesta
+                $responseText = implode("\n---\n", $formattedText);
+
+                return response()->json([
+                    'message' => "Las Cotizaciones del dolar son las siguiente: \n" . $responseText,
+                    'found' => true, 'action' => true], 200);
+
+            } else {
+                return response()->json(['error' => 'Error fetching data'], $response->status());
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Exception occurred: ' . $e->getMessage()], 500);
+        }
+    }
+}
