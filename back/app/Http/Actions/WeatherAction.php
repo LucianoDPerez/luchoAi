bca14567b7a7cdeb5196dc4bea43cb05f41480e7
Luciano D. Perez <lucianoperezvic84@gmail.com>
2024-08-13 12:33:07 -0300

    test new weather api

--- back/app/Http/Actions/WeatherAction.php
+++ back/app/Http/Actions/WeatherAction.php
@@ -13,16 +13,48 @@ class WeatherAction implements ActionInterface
     public function execute(Request $request)
     {
         $data = $request->all();
-        $weather = new Clima();
-        $returnWeather = $weather->getCurrentWeather(Str::ascii($data['city']));
+        $city = urlencode(Str::ascii($data['city'] ?? 'Victoria, Entre Rios, AR'));
 
-        Log::info(json_encode($returnWeather));
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{$city}?unitGroup=metric&key=W34TQR82YWS5U2N2PW9T4XPCP&contentType=json",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        if ($response === false) {
+            Log::error('Error al obtener el clima: ' . curl_error($curl));
+            return response()->json(['error' => 'No se pudo obtener el clima.'], 500);
+        }
+
+        curl_close($curl);
+        $weatherData = json_decode($response, true);
+
+        // Verifica si 'days' existe y tiene al menos un elemento
+        if (!isset($weatherData['days']) || !is_array($weatherData['days']) || count($weatherData['days']) === 0) {
+            Log::error('No se encontraron datos del clima para la ciudad: ' . $city);
+            return response()->json(['error' => 'No se encontraron datos del clima para la ciudad.'], 404);
+        }
+
+        $currentWeather = $weatherData['days'][0];
+
         return response()->json([
-            'message' => 'El pronostico para ' . $data['city'] . " es: \n" .
-                            'Temperatura actual: ' . $returnWeather->temp . "\n" .
-                            'Temperatura minima: ' . $returnWeather->temp_min . "\n" .
-                            'Temperatura maxima: ' . $returnWeather->temp_max . "\n" .
-                            'Porcentaje humedad: ' . $returnWeather->humidity,
-            'found' => true, 'action' => true], 200);
+            'message' => "El pronóstico para {$data['city']} es:\n" .
+                "Temperatura actual: {$currentWeather['temp']}\n" .
+                "Temperatura mínima: {$currentWeather['tempmin']}\n" .
+                "Temperatura máxima: {$currentWeather['tempmax']}\n" .
+                "Porcentaje humedad: {$currentWeather['humidity']}",
+            'found' => true,
+            'action' => true
+        ], 200);
     }
+
 }
