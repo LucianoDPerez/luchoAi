89308cd17b83711e6277b0009c275eb645baabbb
Luciano D. Perez <lucianoperezvic84@gmail.com>
2024-09-22 17:35:12 -0300

    fix cors origin accept all

--- back/routes/api.php
+++ back/routes/api.php
@@ -9,23 +9,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::group(['middleware' => 'cors'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-// routes/api.php
+    // routes/api.php
 
-Route::post('/whisper', [AssistantController::class, 'whisper']);
-Route::post('/text', [AssistantController::class, '__invoke']);
+    Route::post('/whisper', [AssistantController::class, 'whisper']);
+    Route::post('/text', [AssistantController::class, '__invoke']);
 
-Route::post('/alimentos', [AssistantController::class, '__invoke']);
-
-
-Route::post('/blackbox', [BlackboxController::class, 'blackbox']);
-
-Route::post('/google/auth', [GoogleController::class, 'auth']);
-Route::get('/google/create-task', [GoogleController::class, 'createTask']);
+    Route::post('/alimentos', [AssistantController::class, '__invoke']);
 
 
+    Route::post('/blackbox', [BlackboxController::class, 'blackbox']);
+
+    Route::post('/google/auth', [GoogleController::class, 'auth']);
+    Route::get('/google/create-task', [GoogleController::class, 'createTask']);
+
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
