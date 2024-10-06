ad7ab15d4a997cef0dc1e80d9c1d62db7e97f777
Luciano D. Perez <lucianoperezvic84@gmail.com>
2024-10-06 17:20:55 -0300

    fix getAudioService

--- back/app/Http/Middleware/ValidateAudio.php
+++ back/app/Http/Middleware/ValidateAudio.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ValidateAudio
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param Closure(Request): (Response) $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         $request->validate([
