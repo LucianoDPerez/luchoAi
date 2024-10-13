1a6532eb1cce9961911f556300c6240c5ac19eb4
Luciano D. Perez <lucianoperezvic84@gmail.com>
2024-10-13 19:39:31 -0300

    add waapi webhook

--- back/app/Http/Controllers/WaapiController.php
+++ back/app/Http/Controllers/WaapiController.php
@@ -6,10 +6,15 @@ use Illuminate\Http\Request;
 
 class WaapiController extends Controller
 {
-    public function receive()
+    public function receive(Request $request)
     {
         $response = file_get_contents('php://input');
 
-        file_put_contents('receive.txt', $response);
+        if ($response) {
+            file_put_contents('receive.txt', $response);
+            return response()->json(['message' => 'Data received successfully'], 200);
+        } else {
+            return response()->json(['message' => 'No data received'], 400);
+        }
     }
 }
