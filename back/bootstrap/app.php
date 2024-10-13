ec68d50ec79d7f4a61b421c819b306073db16827
Luciano D. Perez <lucianoperezvic84@gmail.com>
2024-10-13 20:42:46 -0300

    add waapi webhook

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -12,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-       /* $middleware->validateCsrfTokens(except: [
+       $middleware->validateCsrfTokens(except: [
             'waapi',
-        ]);*/
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
